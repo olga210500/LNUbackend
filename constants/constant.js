bac5255e20708cb04135a06a6203a1b49b5e01d2
olha kovalisko <olha.kovalisko@knameit.com>
2022-11-23 20:54:50 +0200

    initial commet

---
+++ constants/constant.js
@@ -0,0 +1,11 @@
+const SMTP_PORT=process.env.SMTP_PORT
+const SMTP_HOST=process.env.SMTP_HOST
+const SMTP_USER=process.env.SMTP_USER
+const SMTP_PASSWORD=process.env.SMTP_PASSWORD
+
+export{
+    SMTP_HOST,
+    SMTP_PORT,
+    SMTP_USER,
+    SMTP_PASSWORD
+}
--- services/mailService.js
+++ services/mailService.js
@@ -1,31 +1,45 @@
 import nodemailer from "nodemailer";
+import { SMTP_HOST, SMTP_PORT } from "../constants/constant.js";
 class MailService {
   constructor() {
-    this.transporter = nodemailer.createTransport({
-        host: process.env.SMTP_HOST,
-        port: process.env.SMTP_PORT,
-        secure: false,
-        auth: {
-            user: process.env.SMTP_USER,
-            pass: process.env.SMTP_PASSWORD
-        }
-    })
-}
+    this.data = {
+      host: SMTP_HOST,
+      port: SMTP_PORT,
+      secure: false,
+      auth: {
+        user: "olakovalisko@gmail.com",
+        pass: "pscphohsnrtcuyrm",
+      },
+    };
 
-async sendActivationMail(to, link) {
-    await this.transporter.sendMail({
-        from: process.env.SMTP_USER,
-        to,
-        subject: 'Account activation ' + process.env.API_URL,
-        text: '',
-        html:
-            `
+    this.transporter = nodemailer.createTransport(this.data);
+  }
+
+  async sendActivationMail(to, link) {
+    console.log(process.env.SMTP_HOST, SMTP_PORT);
+    const options = {
+      host: process.env.SMTP_HOST,
+      port: process.env.SMTP_PORT,
+      secure: false,
+      auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASSWORD,
+      },
+    };
+
+    const transporter = nodemailer.createTransport(options);
+    await transporter.sendMail({
+      from: process.env.SMTP_USER,
+      to,
+      subject: "Account activation " + process.env.API_URL,
+      text: "",
+      html: `
                 <div>
                     <h1>Для підтвердження авторизації перейдіть за посиланням</h1>
                     <a href="${link}">${link}</a>
                 </div>
-            `
-    })
-}
+            `,
+    });
+  }
 }
 export default new MailService();
